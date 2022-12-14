733369ec796fc98a5fc14142cded4d87884cc333
Muhammad Habib Jawady <mohamedhabib.jaouadi@edu.isetcom.tn>
2022-12-14 12:16:44 +0100

    Implementing Tree and IsEmpty check 🧾

--- Tree/Tree.go
+++ Tree/Tree.go
@@ -7,6 +7,17 @@ type TNode struct {
 	Children []*TNode
 }
 
+// NewTree creates an empty Tree
+func NewTree() *TNode {
+	return &TNode{0, []*TNode{}}
+}
+
+// IsEmpty checks whether a given tree is empty or not.
+func (t *TNode) IsEmpty() bool {
+	return (*t).Value == 0 && len((*t).Children) == 0
+}
+
+// NewNode creates a new node and returns its address.
 func NewNode(val rune) *TNode {
 	var tmp *TNode = &TNode{Value: val, Children: []*TNode{}}
 	return tmp
@@ -17,7 +28,7 @@ func (t *TNode) AddChild(node *TNode) {
 	t.Children = append(t.Children, node)
 }
 
-// Display output the values present in the tree in out.
+// String Display output the values present in the tree in out.
 func String(node *TNode, out *string) {
 	*out = (*out) + fmt.Sprintf("%c ", (*node).Value)
 	for _, n := range (*node).Children {
