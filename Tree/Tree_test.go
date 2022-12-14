4b73a3fd18278e728fe7e799b831d0aa32a171de
Muhammad Habib Jawady <mohamedhabib.jaouadi@edu.isetcom.tn>
2022-12-14 12:15:52 +0100

    Added tests for Tree DS👓

--- Tree/Tree_test.go
+++ Tree/Tree_test.go
@@ -2,12 +2,28 @@ package Tree
 
 import "testing"
 
+func TestIsEmpty(t *testing.T) {
+	type testEmpty struct {
+		value    bool
+		expected bool
+	}
+	tree1 := NewTree()
+	tree2 := NewNode('a')
+	var tests = []testEmpty{
+		{tree1.IsEmpty(), true},
+		{tree2.IsEmpty(), false},
+	}
+	for _, test := range tests {
+		if test.value != test.expected {
+			t.Errorf("Output %t not equal to expected %t", test.value, test.expected)
+		}
+	}
+}
 func TestNewNode(t *testing.T) {
 	type newTest struct {
 		value    rune
 		expected string
 	}
-
 	var testNodes = []newTest{
 		{'a', "a "},
 		{'A', "A "},
@@ -26,10 +42,8 @@ func TestAddChild(t *testing.T) {
 	tree := NewNode('a')
 	nodeB := NewNode('B')
 	tree.AddChild(nodeB)
-
 	var got string
 	String(tree, &got)
-
 	want := "a B "
 	if got != want {
 		t.Errorf("Output %q not equal to expected %q", got, want)
